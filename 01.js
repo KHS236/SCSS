7aee59fb3b0f9305d2a65bc8374d4c793ae31c6e
KHS236 <fdsa5104@gmail.com>
2025-10-23 15:21:31 +0900

    .

---
+++ 01.js
@@ -0,0 +1,28 @@
+import "./01.scss"
+
+console.log("01-JS")
+
+
+// 1-2 js ------------------------------------------------------
+const btn = document.querySelector('.container2 .btn')
+let toggle = true;
+btn.addEventListener('click',()=>{
+
+    if(toggle){
+        btn.classList.add('active')
+    }else{
+        btn.classList.remove('active')
+    }
+    toggle = !toggle
+})
+
+//  ----------------------------------------------------
+
+
+
+
+
+
+
+
+
